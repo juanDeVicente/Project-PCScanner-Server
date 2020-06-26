5ac834a6a6c8faac4329994688982fea9110e49f
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-06-26 11:21:36 +0200

    Añadida la funcionalidad de apagar y reiniciar el PC

--- Server Windows/src/Program.cs
+++ Server Windows/src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Management;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,12 +60,20 @@ namespace Server_Windows
                 else if ((req.HttpMethod == "GET") && (req.Url.AbsolutePath == "/properties"))
                     WriteResponse(resp, propertiesController.HandlePath(""));
                 else if ((req.HttpMethod == "GET") && (req.Url.AbsolutePath == "/shutdown"))
-                    runServer = false;
-                else if(req.HttpMethod == "GET")
+                {
+                    WriteResponse(resp, new Model[0]);
+                    InvokeWin32ShutdownMethod("1"); //FIXME puede ser que tenga que poner lo de force shutdown
+                }
+                else if ((req.HttpMethod == "GET") && (req.Url.AbsolutePath == "/reboot"))
+                {
+                    WriteResponse(resp, new Model[0]);
+                    InvokeWin32ShutdownMethod("2");
+                }
+                else if (req.HttpMethod == "GET")
                     WriteResponse(resp, new Model[0]);
                 else
                 {
-                    resp.StatusCode = (int) HttpStatusCode.NotFound;
+                    resp.StatusCode = (int)HttpStatusCode.NotFound;
                     resp.Close();
                 } 
             }
@@ -99,7 +108,7 @@ namespace Server_Windows
 
             inboundRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
 
-            inboundRule.Protocol = 6;
+            inboundRule.Protocol = 6; //TCP
 
             inboundRule.LocalPorts = port;
             
@@ -110,5 +119,19 @@ namespace Server_Windows
             INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
             firewallPolicy.Rules.Add(inboundRule);
         }
-	}
+        public static void InvokeWin32ShutdownMethod(string flag)
+        {
+			ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                     mcWin32.GetMethodParameters("Win32Shutdown");
+
+            mboShutdownParams["Flags"] = flag; 
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+				_ = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+        }
+    }
 }
